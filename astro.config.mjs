cd5e33dfbcaa47937f251b94e0bbb1b792ddd31b
mariavaldiviezopingo <luisavaldiviezo24@gmail.com>
2025-07-05 12:36:27 -0500

    configurando despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://serviciosvaldiser.github.io',
+  base: 'serviciosvaldiser.github.io',
   devToolbar: {
     enabled: false
   },
